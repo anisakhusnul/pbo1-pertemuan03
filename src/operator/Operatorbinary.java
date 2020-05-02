ddf328310a465c82d8a0cce247a13041f9b1487d
anisakhusnul <62650996+anisakhusnul@users.noreply.github.com>
2020-05-02 00:40:26 -0700

    Operator Binary Assigment

---
+++ src/operator/Operatorbinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class Operatorbinary {
+
+    //operator (=) sebagai assigmebt/penugasan
+    //menempatkan nilai kedalam variable
+    //baris-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "ANISA KHUSNUL KHOTIMAH" ;
+    int jumlahSks = 24;
+    double indeksPrestasiKomulatif = 3.89;
+
+
+}
