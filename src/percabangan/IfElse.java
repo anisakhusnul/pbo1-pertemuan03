fa6441eee993f4ea24f4319183c2425e3aac4eb6
anisakhusnul <62650996+anisakhusnul@users.noreply.github.com>
2020-05-02 05:34:02 -0700

    percabangan nisa

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -7,8 +7,12 @@ public class IfElse {
 
         if(uangDiDompet<totalBelanja) {
             System.out.println("Uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja) {
+            double angsul = uangDiDompet - totalBelanja;
+            System.out.println("Uang cukup, angsul : " + angsul);
         }else{
-            System.out.println("Uang cukup, selamat menikmati");
+            System.out.println("Uang pas.. Uwu..");
+
         }
     }
 }
