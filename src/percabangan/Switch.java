4b1dbfeb9ce898f613a7bd1a99912e5d0ceed937
anisakhusnul <62650996+anisakhusnul@users.noreply.github.com>
2020-05-02 05:56:46 -0700

    percabangan nisa nisa nisa

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -1,23 +1,27 @@
 package percabangan;
 
 public class Switch {
-    int jalanJalan=3;
-    switch(jalanJalan)
-
-    {
-        case 1:
-            System.out.println("urang lapas");
+    public static void main(String[] args){
+    char nilai ='D'
+    switch(nilai){
+        case 'A':
+            System.out.println("mhs : \"Teri lapama Kasih pak\"");
+            System.out.println("Dosen : \"Selamat ya !!\"");
             break;
-        case 2:
-            System.out.println("urang acayan");
+        case 'B':
+            System.out.println("mhs : \"Teri lapama Kasih pak\"");
+            System.out.println("Dosen : \"Selamat ya !!\"");
             break;
-        case 3:
-            System.out.println("urang nyamuk");
-            break;
-        case 2:
-            System.out.println("urang dit");
+        case 'C':
+            System.out.println("mhs : \"Teri lapama Kasih pak\"");
+            System.out.println("Dosen : \"Selamat ya !!\"");
+            System.out.println("Mhs :\"");
             break;
         default:
+            System.out.println("makhluk halus");
+            System.out.println("makhluk halus");
+            System.out.println("makhluk halus");
+            System.out.println("makhluk halus");
             System.out.println("makhluk halus");
             break;
         }
