b1dc0967d4d4f92c19f2b0a07d42ceaad9cd6a60
anisakhusnul <62650996+anisakhusnul@users.noreply.github.com>
2020-05-02 00:48:37 -0700

    Operator Binary Aritmatika

--- src/operator/Operatorbinary.java
+++ src/operator/Operatorbinary.java
@@ -6,9 +6,19 @@ public class Operatorbinary {
     //menempatkan nilai kedalam variable
     //baris-7 adalah comment/komentar yang tidak dijalankan oleh program
 
-    String nama = "ANISA KHUSNUL KHOTIMAH" ;
+    String nama = "ANISA KHUSNUL KHOTIMAH";
     int jumlahSks = 24;
     double indeksPrestasiKomulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9 + 3);
+        System.out.println(9 - 3);
+        System.out.println(9 * 3);
+        System.out.println(9 / 3);
+        System.out.println(9 % 3);
+        System.out.println(9 % 2);
+    }
+}
 
 
 }
