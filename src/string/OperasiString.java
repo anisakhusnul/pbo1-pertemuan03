f0ae03e4f16b2451ae0f826cee942aba68f7a906
anisakhusnul <62650996+anisakhusnul@users.noreply.github.com>
2020-05-02 00:06:18 -0700

    Buat Class OperasiString

--- src/string/OperasiString.java
+++ src/string/OperasiString.java
@@ -2,8 +2,8 @@ package string;
 
 public class OperasiString {
     public static void main(String[] args) {
-        String kota = "Yogyakarta" ;
+        String kota = "Yogyakarta";
 
         System.out.println(kota);
     }
-}   
+}
