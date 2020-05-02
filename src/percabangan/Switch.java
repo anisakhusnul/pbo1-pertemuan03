834cb20fc9164d1bb0170d17acbf95418fc55a0b
anisakhusnul <62650996+anisakhusnul@users.noreply.github.com>
2020-05-02 05:43:00 -0700

    percabangan nisa nisa

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,25 @@
+package percabangan;
+
+public class Switch {
+    int jalanJalan=3;
+    switch(jalanJalan)
+
+    {
+        case 1:
+            System.out.println("urang lapas");
+            break;
+        case 2:
+            System.out.println("urang acayan");
+            break;
+        case 3:
+            System.out.println("urang nyamuk");
+            break;
+        case 2:
+            System.out.println("urang dit");
+            break;
+        default:
+            System.out.println("makhluk halus");
+            break;
+        }
+    }
+}
